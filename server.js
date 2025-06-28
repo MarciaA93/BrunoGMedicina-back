0170a522e0de1c20a7f4a181ee9bdfb7db71f0fd
mmontivero <mmontivero@tgv.com.ar>
2025-06-28 16:34:29 -0300

    cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://brunograttonni.netlify.app'],
   credentials: true
 }));
 
